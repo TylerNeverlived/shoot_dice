db532013b8e3258f890c35fbcb6e891192a2e2c5
TylerNeverlived <107303031+TylerNeverlived@users.noreply.github.com>
2022-09-18 14:19:41 +0300

    add change plaer, total score

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
 'use strict';
+
+// Element selection
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1');
+const current0Element = document.getElementById('current--0');
+const current1Element = document.getElementById('current--1');
+const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
+
+//Game initial conditions
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
+
+const totalScores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+const swichActivePlayer = function () {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0Element.classList.toggle('player--active');
+  player1Element.classList.toggle('player--active');
+};
+//Roll the dice
+btnRoll.addEventListener('click', function () {
+  // 1. Generate a random number
+  const diceNumber = Math.trunc(Math.random() * 6) + 1;
+  console.log(diceNumber);
+
+  // 2. Display number on the dice
+  diceElement.classList.remove('hidden');
+  diceElement.src = `dice${diceNumber}.png`;
+
+  // 3. If the number is 1, swich to the next player, if not - add number to the current score
+  if (diceNumber !== 1) {
+    currentScore += diceNumber;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    swichActivePlayer();
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  // 1. Add current score to active total score
+  totalScores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    totalScores[activePlayer];
+  // 2. If total score of active plaer >= 100, active player won, if not - swich activ player
+  swichActivePlayer();
+});
